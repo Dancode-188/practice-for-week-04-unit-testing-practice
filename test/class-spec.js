98463539a8f17f98fbdfd796c83acfb50f01c1a4
Dancode-188 <danbitengo@gmail.com>
2024-09-21 06:53:16 +0300

    Practiced uni tests w/ Mocha and Chai

--- test/class-spec.js
+++ test/class-spec.js
@@ -6,29 +6,34 @@ const { Word } = require("../class");
 describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      const word = new Word("hello");
+      expect(word.word).to.equal("hello");
     });
   
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      const word = new Word("hello");
+      expect(word.word).to.equal("hello");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      const word = new Word("hello");
+      expect(word.removeVowels()).to.equal("hll");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      const word = new Word("hello");
+      expect(word.removeConsonants()).to.equal("eo");
     });
   });
   
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      const word = new Word("hello");
+      expect(word.pigLatin()).to.equal("ellohay");
     });
   });
 });
